f440ea2a327a12e6d5e4a58c9ffb55b2ee811a3f
dannytoan <dannytoan1@gmail.com>
2022-05-26 12:43:34 -0700

    finished phase 3

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,28 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+// Test route - if there is no user, the route will return an error
+    // otherwise it will return the session user's information
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//       return res.json(req.user);
+//     }
+//   );
+
+router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+        where: {
+          username: 'Demo-lition'
+        }
+      });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  }));
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
