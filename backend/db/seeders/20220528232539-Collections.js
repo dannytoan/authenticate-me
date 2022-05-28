7a26995270e4f0d5f8e09e7b8d0ac20444a29aa1
dannytoan <dannytoan1@gmail.com>
2022-05-28 16:27:21 -0700

    added seeders for Photos and Collections

---
+++ backend/db/seeders/20220528232539-Collections.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkInsert('Collections', [{
+     title: 'My Moodboard Fall 2022',
+   }], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      */
+   return queryInterface.bulkDelete('Collections', null, {});
+  }
+};
