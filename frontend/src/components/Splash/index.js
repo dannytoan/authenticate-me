e283929d4e62b3a740436807927bc4fd1af91df7
dannytoan <dannytoan1@gmail.com>
2022-05-30 03:38:03 -0700

    added github in nav

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,13 +1,18 @@
-import "./Splash.css"
+import "./Splash.css";
 
 function Splash() {
-
-    return (
-        <div>
-            <div>hi</div>
+  return (
+    <div id="splash-body">
+      <div>
+        <div id="mid-header-container">
+          <img
+            id="mid-header-photo"
+            src="https://images.lifestyleasia.com/wp-content/uploads/sites/6/2020/10/06075646/balmain-header-Cropped.jpg"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-
 
 export default Splash;
