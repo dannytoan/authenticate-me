f5f201fb557ce63260e64e47a7cad4c0f5a4fd03
dannytoan <dannytoan1@gmail.com>
2022-05-31 00:07:39 -0700

    added signup button functionality and resized splashed page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -10,11 +10,16 @@ function Splash() {
             src="https://images.lifestyleasia.com/wp-content/uploads/sites/6/2020/10/06075646/balmain-header-Cropped.jpg"
           /> */}
           <video className="mid-header-video" autoPlay playsinline muted loop>
-            <source type="video/mp4" src="https://res.cloudinary.com/www-eliesaab-com/video/upload/c_scale,h_1200,q_auto:good/v1644226949/HC_SS22_Banner_video_1_z56lug.mp4"/>
+            <source
+              type="video/mp4"
+              src="https://res.cloudinary.com/www-eliesaab-com/video/upload/c_scale,h_1200,q_auto:good/v1644226949/HC_SS22_Banner_video_1_z56lug.mp4"
+            />
           </video>
           <div id="video-text-container">
             <h2 id="video-text-title">Find your inspiration.</h2>
-            <button id="video-signup-button">Sign Up</button>
+            <a href="/signup">
+              <button id="video-signup-button">Start for Free</button>
+            </a>
           </div>
         </div>
       </div>
