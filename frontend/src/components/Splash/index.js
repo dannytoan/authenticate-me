8bc521269e069d301c4b1d72d64250b24ca41626
dannytoan <dannytoan1@gmail.com>
2022-06-03 16:14:06 -0700

    finished styling splash demo user

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,6 +1,6 @@
 import "./Splash.css";
 import { useSelector, useDispatch } from "react-redux";
-import * as sessionActions from "../../store/session"
+import * as sessionActions from "../../store/session";
 import { useHistory } from "react-router-dom";
 
 function Splash() {
@@ -10,9 +10,11 @@ function Splash() {
 
   const handleDemoSubmit = (e) => {
     const credential = "demo@user.io";
-    const password = "password"
-    return dispatch(sessionActions.login({ credential, password })).then(()=> history.push('/'))
-  }
+    const password = "password";
+    return dispatch(sessionActions.login({ credential, password })).then(() =>
+      history.push("/")
+    );
+  };
 
   return (
     <div id="splash-body">
@@ -22,32 +24,48 @@ function Splash() {
             id="mid-header-photo"
             src="https://images.lifestyleasia.com/wp-content/uploads/sites/6/2020/10/06075646/balmain-header-Cropped.jpg"
           /> */}
-          {(!sessionUser) ? <video className="mid-header-video" autoPlay playsInline muted loop>
-            <source
-              type="video/mp4"
-              src="https://res.cloudinary.com/www-eliesaab-com/video/upload/c_scale,h_1200,q_auto:good/v1644226949/HC_SS22_Banner_video_1_z56lug.mp4"
-            />
-          </video> : <video className="mid-header-video" autoPlay playsInline muted loop>
-            <source
-            type="video/mp4"
-            src="https://res.cloudinary.com/matchaprince/video/upload/v1654204077/final_6298f521360d2e00d54e7b11_531959_m00i0z.mp4"
-            />
-            </video>}
+          {!sessionUser ? (
+            <video className="mid-header-video" autoPlay playsInline muted loop>
+              <source
+                type="video/mp4"
+                src="https://res.cloudinary.com/www-eliesaab-com/video/upload/c_scale,h_1200,q_auto:good/v1644226949/HC_SS22_Banner_video_1_z56lug.mp4"
+              />
+            </video>
+          ) : (
+            <video className="mid-header-video" autoPlay playsInline muted loop>
+              <source
+                type="video/mp4"
+                src="https://res.cloudinary.com/matchaprince/video/upload/v1654204077/final_6298f521360d2e00d54e7b11_531959_m00i0z.mp4"
+              />
+            </video>
+          )}
           <div id="video-text-container">
-            <h2 id="video-text-title">{(!sessionUser) ? "Find your inspiration." : "Begin the Inspiration."}</h2>
-            {(!sessionUser) ? <a href="/signup">
-              <button id="video-signup-button">Start for Free</button>
-            </a> : <a href="/photos">
-              <button id="video-signup-button">Browse Looks</button>
-            </a>}
-            {/* {sessionLinks} */}
+            <h2 id="video-text-title">
+              {!sessionUser
+                ? "Find your inspiration."
+                : "Begin the Inspiration."}
+            </h2>
+            <div id="demo-user-btns">
+              {!sessionUser ? (
+                <a href="/signup">
+                  <button id="video-signup-button">Start for Free</button>
+                </a>
+              ) : (
+                <a href="/photos">
+                  <button id="browse-looks-button">Browse Looks</button>
+                </a>
+              )}
+              {!sessionUser ? (
+                <form id="demo-user-container" onSubmit={handleDemoSubmit}>
+                  <button id="demo-user-btn">Demo User</button>
+                </form>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </div>
       </div>
-          {(!sessionUser) ? <form onSubmit={handleDemoSubmit}>
-            <button>Demo User</button>
-          </form> : <></>}
-
     </div>
   );
 }
