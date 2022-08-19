2ca10a95c231da409f8c95698572b9e9a6636031
dannytoan <dannytoan1@gmail.com>
2022-08-19 13:42:10 -0700

    added error valids to both new and edit comments

--- frontend/src/components/EditComment/index.js
+++ frontend/src/components/EditComment/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { editComment, deleteComment } from "../../store/comments";
 
@@ -6,6 +6,19 @@ function EditComment({ commentData }) {
   const dispatch = useDispatch();
   const [comment, setComment] = useState(commentData.comment);
   const [showEdit, setShowEdit] = useState(false);
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    const errors = [];
+
+    if (comment.length >= 250) {
+      errors.push("Comment must not exceed 250 characters.");
+    } else if (comment.length <= 10) {
+      errors.push("Comment must be between 10 to 250 characters.");
+    }
+
+    setErrors(errors);
+  }, [comment]);
 
   const handleEdit = (e) => {
     e.preventDefault();
@@ -18,6 +31,7 @@ function EditComment({ commentData }) {
 
     if (editedComment) {
       setShowEdit(false);
+      window.location.reload(false)
     }
   };
 
@@ -44,6 +58,13 @@ function EditComment({ commentData }) {
           required
           className={showEdit ? "showEdit" : "hideShowEdit"}
           />
+          <div id="new-comments-error-valids" className={showEdit ? "showEdit" : "hideShowEdit"}>
+            {errors.map((error, idx) => (
+              <div key={idx}>
+                {error}
+              </div>
+            ))}
+          </div>
           <div id="edit-comment-buttons-ctnr">
 
         <button
--- frontend/src/components/NewCommentForm/index.js
+++ frontend/src/components/NewCommentForm/index.js
@@ -1,45 +1,65 @@
 import { createComment } from "../../store/comments";
 import { useDispatch, useSelector } from "react-redux";
-import "./NewCommentForm.css"
-import { useState } from "react";
+import "./NewCommentForm.css";
+import { useState, useEffect } from "react";
 
-function NewCommentForm({photoId}) {
-    const dispatch = useDispatch();
+function NewCommentForm({ photoId }) {
+  const dispatch = useDispatch();
 
-    const sessionUser = useSelector((state) => state.session.user);
-    const [comment, setComment] = useState("");
+  const sessionUser = useSelector((state) => state.session.user);
+  const [comment, setComment] = useState("");
+  const [errors, setErrors] = useState([]);
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    const errors = [];
 
-        const payload = {
-            userId: sessionUser.id,
-            photoId: photoId,
-            comment
-        }
-
-       const newComment = dispatch(createComment(payload))
-
-       if (newComment) {
-        window.location.reload(false)
-       }
+    if (comment.length >= 250) {
+      errors.push("Comment must not exceed 250 characters.");
+    } else if (comment.length <= 10) {
+      errors.push("Comment must be between 10 to 250 characters.");
     }
 
-    return (
-        <div id="new-comment-form-ctnr">
-            <form id="new-comment-form" onSubmit={handleSubmit}>
-                <textarea
-                type="text"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                placeholder="Add a comment"
-                id="new-comment-form-textarea"
-                required
-                />
-                <button id="new-comment-form-submit-btn">Submit</button>
-            </form>
-        </div>
-    )
+    setErrors(errors);
+  }, [comment]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const payload = {
+      userId: sessionUser.id,
+      photoId: photoId,
+      comment,
+    };
+
+    const newComment = dispatch(createComment(payload));
+
+    if (newComment) {
+      window.location.reload(false);
+    }
+  };
+
+  return (
+    <div id="new-comment-form-ctnr">
+      <form id="new-comment-form" onSubmit={handleSubmit}>
+        <textarea
+          type="text"
+          value={comment}
+          onChange={(e) => setComment(e.target.value)}
+          placeholder="Add a comment"
+          id="new-comment-form-textarea"
+          required
+          />
+          <div id="new-comments-error-valids">
+            {errors.map((error, idx) => (
+              <div key={idx}>
+                {error}
+              </div>
+            ))}
+          </div>
+        <button id="new-comment-form-submit-btn" disabled={errors.length}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default NewCommentForm;
