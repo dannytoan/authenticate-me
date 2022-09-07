92658ac4c0cf0b9f9644c60f0a3205811b97f680
dannytoan <dannytoan1@gmail.com>
2022-09-06 20:18:11 -0700

    trying aws upload

--- backend/routes/api/addLook.js
+++ backend/routes/api/addLook.js
@@ -6,33 +6,17 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const { requireAuth } = require('../../utils/auth')
 
+const { singlePublicFileUpload, singleMulterUpload } = require("../../awsS3")
 
 const router = express.Router();
 
 
-// VALIDATE SUBMITTING A PHOTO
-// const validatePostPhoto = [
-//   check("imageUrl")
-//     .exists({ checkFalsy: true })
-//     .withMessage("Please provide an Image URL.")
-//     .isURL({ checkFalsy: true })
-//     .withMessage("Please provide a valid Image URL."),
-//   check("description")
-//     .exists({ checkFalsy: true })
-//     .withMessage("Please provide a title.")
-//     .isLength({ min: 1, max: 256 })
-//     .withMessage(
-//       "Title must contain at least 1 and no more than 256 characters."
-//     ),
-
-//   handleValidationErrors,
-// ];
-
 router.post(
   "/",
+  singleMulterUpload("image"),
   asyncHandler(async function (req, res) {
     const photo = await db.Photo.create(req.body);
-
+    const newPhotoUrl = await singlePublicFileUpload(req.file);
       return res.json(photo);
   })
 );
